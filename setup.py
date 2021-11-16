6217ff79a8cdfaf1ca6608ff40abd0fa5391857a
zhouzaida <zhouzaida@163.com>
2021-11-16 23:22:50 +0800

    [Fix] Automatically install runtime dependencies

--- setup.py
+++ setup.py
@@ -164,8 +164,14 @@ if __name__ == '__main__':
         author='MMFlow Contributors',
         author_email='openmmlab@gmail.com',
         license='Apache License 2.0',
-        setup_requires=['pytest-runner', 'numpy'],
-        tests_require=['pytest'],
-        install_requires=parse_requirements('requirements/build.txt'),
+        setup_requires=parse_requirements('requirements/build.txt'),
+        tests_require=parse_requirements('requirements/tests.txt'),
+        install_requires=parse_requirements('requirements/runtime.txt'),
+        extras_require={
+            'all': parse_requirements('requirements.txt'),
+            'tests': parse_requirements('requirements/tests.txt'),
+            'build': parse_requirements('requirements/build.txt'),
+            'optional': parse_requirements('requirements/optional.txt'),
+        },
         ext_modules=[],
         zip_safe=False)
