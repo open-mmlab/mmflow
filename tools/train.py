a59279ae1e5d0df33c51ba14d3f1990bc7537521
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2022-01-06 20:15:47 +0800

    [Features] Support auto-resume (#71)
    * [Features] Support auto-resume
    
    * add comments
    
    * modify docstring
    
    * modify train tools

--- tools/train.py
+++ tools/train.py
@@ -28,6 +28,10 @@ def parse_args():
     parser.add_argument(
         '--resume-from', help='the checkpoint file to resume from')
     parser.add_argument(
+        '--auto-resume',
+        action='store_true',
+        help='resume from the latest checkpoint automatically')
+    parser.add_argument(
         '--no-validate',
         action='store_true',
         help='whether not to evaluate the checkpoint during training')
@@ -93,6 +97,7 @@ def main():
         cfg.load_from = args.load_from
     if args.resume_from is not None:
         cfg.resume_from = args.resume_from
+    cfg.auto_resume = args.auto_resume
     if args.gpu_ids is not None:
         cfg.gpu_ids = args.gpu_ids
     else:
