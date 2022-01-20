d2f249c785e4d62de80be52f51d330bdba640d9c
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2022-01-20 21:55:00 +0800

    Add multi-processes script (#79)
    * Add multi-processes script
    
    * update log

--- mmflow/utils/__init__.py
+++ mmflow/utils/__init__.py
@@ -1,5 +1,9 @@
 from .collect_env import collect_env
 from .logger import get_root_logger
 from .misc import find_latest_checkpoint
+from .set_env import setup_multi_processes
 
-__all__ = ['collect_env', 'get_root_logger', 'find_latest_checkpoint']
+__all__ = [
+    'collect_env', 'get_root_logger', 'find_latest_checkpoint',
+    'setup_multi_processes'
+]
