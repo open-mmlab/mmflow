6b8db940e5643a97af7340fc6de652e45443c470
MeowZheng <meowzheng@outlook.com>
2022-03-24 15:16:08 +0800

    [Fix] Sync seed for dist sampler

--- mmflow/datasets/samplers/distributed_sampler.py
+++ mmflow/datasets/samplers/distributed_sampler.py
@@ -15,9 +15,6 @@ class DistributedSampler(_DistributedSampler):
     """DistributedSampler inheriting from
     `torch.utils.data.DistributedSampler`.
 
-    In pytorch of lower versions, there is no `shuffle` argument. This child
-    class will port one to DistributedSampler.
-
     Args:
         datasets (Dataset): the dataset will be loaded.
         num_replicas (int, optional): Number of processes participating in
