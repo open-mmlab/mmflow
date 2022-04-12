d29608e4599bbbe619a85a1a53021e40c6b43c90
MeowZheng <meowzheng@outlook.com>
2022-04-12 23:01:17 +0800

    flow_from_bytes

--- mmflow/datasets/utils/flow_io.py
+++ mmflow/datasets/utils/flow_io.py
@@ -2,7 +2,7 @@
 
 import re
 from io import BytesIO
-from typing import Tuple, Union
+from typing import Tuple
 
 import cv2
 import matplotlib.pyplot as plt
@@ -144,13 +144,32 @@ def write_flow_kitti(uv: np.ndarray, filename: str):
     cv2.imwrite(filename, uv[..., ::-1])
 
 
-def flow_from_bytes(content: bytes) -> ndarray:
+def flow_from_bytes(content: bytes, suffix: str = 'flo') -> ndarray:
     """Read dense optical flow from bytes.
 
     .. note::
         This load optical flow function works for FlyingChairs, FlyingThings3D,
         Sintel, FlyingChairsOcc datasets, but cannot load the data from
         ChairsSDHom.
+
+    Args:
+        content (bytes): Optical flow bytes got from files or other streams.
+
+    Returns:
+        ndarray: Loaded optical flow with the shape (H, W, 2).
+    """
+
+    assert suffix in ('flo', 'pfm'), 'suffix of flow file must be `flo` '\
+        f'or `pfm`, but got {suffix}'
+
+    if suffix == 'flo':
+        return flo_from_bytes(content)
+    else:
+        return pfm_from_bytes(content)
+
+
+def flo_from_bytes(content: bytes):
+    """Decode bytes based on flo file.
 
     Args:
         content (bytes): Optical flow bytes got from files or other streams.
@@ -174,24 +193,17 @@ def flow_from_bytes(content: bytes) -> ndarray:
     return flow
 
 
-def read_pfm(file: Union[bytes, str]) -> np.ndarray:
+def pfm_from_bytes(content: bytes) -> np.ndarray:
     """Load the file with the suffix '.pfm'.
 
-    This function is modified from
-    https://lmb.informatik.uni-freiburg.de/resources/datasets/IO.py
-    Copyright (c) 2011, LMB, University of Freiburg.
-
     Args:
-        file (str, bytes): The file name will be loaded, or optical flow bytes
-            got from files or other streams.
+        content (bytes): Optical flow bytes got from files or other streams.
 
     Returns:
         ndarray: The loaded data
     """
-    if isinstance(file, str):
-        file = open(file, 'rb')
-    else:
-        file = BytesIO(file)
+
+    file = BytesIO(content)
 
     color = None
     width = None
@@ -221,6 +233,54 @@ def read_pfm(file: Union[bytes, str]) -> np.ndarray:
         endian = '>'  # big-endian
 
     data = np.frombuffer(file.read(), endian + 'f')
+    shape = (height, width, 3) if color else (height, width)
+
+    data = np.reshape(data, shape)
+    data = np.flipud(data)
+    return data[:, :, :-1]
+
+
+def read_pfm(file: str) -> np.ndarray:
+    """Load the file with the suffix '.pfm'.
+
+    This function is modified from
+    https://lmb.informatik.uni-freiburg.de/resources/datasets/IO.py
+    Copyright (c) 2011, LMB, University of Freiburg.
+    Args:
+        file (str): The file name will be loaded
+    Returns:
+        ndarray: The loaded data
+    """
+    file = open(file, 'rb')
+
+    color = None
+    width = None
+    height = None
+    scale = None
+    endian = None
+
+    header = file.readline().rstrip()
+    if header.decode('ascii') == 'PF':
+        color = True
+    elif header.decode('ascii') == 'Pf':
+        color = False
+    else:
+        raise Exception('Not a PFM file.')
+
+    dim_match = re.match(r'^(\d+)\s(\d+)\s$', file.readline().decode('ascii'))
+    if dim_match:
+        width, height = list(map(int, dim_match.groups()))
+    else:
+        raise Exception('Malformed PFM header.')
+
+    scale = float(file.readline().decode('ascii').rstrip())
+    if scale < 0:  # little-endian
+        endian = '<'
+        scale = -scale
+    else:
+        endian = '>'  # big-endian
+
+    data = np.fromfile(file, endian + 'f')
     shape = (height, width, 3) if color else (height, width)
 
     data = np.reshape(data, shape)
