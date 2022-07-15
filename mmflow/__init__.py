a5755d2a5763ac1b686641073a03f9ca45b8f43d
Zachary-66 <1032903371@qq.com>
2022-07-15 11:24:57 +0800

    set mmcv version

--- mmflow/__init__.py
+++ mmflow/__init__.py
@@ -17,12 +17,12 @@ def digit_version(version_str):
 
 
 mmcv_minimum_version = '1.3.15'
-mmcv_maximum_version = '1.6.0'
+mmcv_maximum_version = '1.7.0'
 mmcv_version = digit_version(mmcv.__version__)
 
 assert (mmcv_version >= digit_version(mmcv_minimum_version)
-        and mmcv_version <= digit_version(mmcv_maximum_version)), \
+        and mmcv_version < digit_version(mmcv_maximum_version)), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
-    f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'
+    f'Please install mmcv>={mmcv_minimum_version}, <{mmcv_maximum_version}.'
 
 __all__ = ['__version__', 'short_version']
