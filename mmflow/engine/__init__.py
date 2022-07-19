94b524a6ba1eda6a072f2ce7615be3cacdb0ab72
MeowZheng <meowzheng@outlook.com>
2022-07-19 16:54:06 +0800

    [Fix] Fix visualization imports

--- mmflow/engine/__init__.py
+++ mmflow/engine/__init__.py
@@ -3,9 +3,10 @@ from .hooks import FlowVisualizationHook, LiteFlowNetStageLoadHook
 from .loops import MultiTestLoop, MultiValLoop
 from .schedulers import (MultiStageLR, MultiStageMomentum,
                          MultiStageParamScheduler)
+from .visualization import FlowLocalVisualizer
 
 __all__ = [
     'LiteFlowNetStageLoadHook', 'FlowVisualizationHook', 'MultiTestLoop',
     'MultiValLoop', 'MultiStageLR', 'MultiStageMomentum',
-    'MultiStageParamScheduler'
+    'MultiStageParamScheduler', 'FlowLocalVisualizer'
 ]
