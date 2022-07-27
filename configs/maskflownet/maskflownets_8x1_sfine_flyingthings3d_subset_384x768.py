a58dd245718a47c83a7c4c5e3d491666af9f2c98
Miao Zheng <76149310+MeowZheng@users.noreply.github.com>
2022-07-27 12:33:27 +0800

    [Fix] MaskFlowNets optim (#194)

--- configs/maskflownet/maskflownets_8x1_sfine_flyingthings3d_subset_384x768.py
+++ configs/maskflownet/maskflownets_8x1_sfine_flyingthings3d_subset_384x768.py
@@ -4,7 +4,9 @@ _base_ = [
     '../_base_/schedules/schedule_s_fine.py', '../_base_/default_runtime.py'
 ]
 
-optimizer = dict(type='Adam', lr=0.0001, weight_decay=0., betas=(0.9, 0.999))
-
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='Adam', lr=0.0001, weight_decay=0., betas=(0.9, 0.999)))
 # Train on FlyingChairs and finetune on FlyingThings3D_subset
 load_from = 'https://download.openmmlab.com/mmflow/maskflownet/maskflownets_8x1_slong_flyingchairs_384x448.pth'  # noqa
